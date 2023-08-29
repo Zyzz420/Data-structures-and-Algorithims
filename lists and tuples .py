2dbd5df9609ba05570e62514516018e205ad9f5e
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-29 01:45:23 -0400

    Assigment done

--- lists and tuples .py
+++ lists and tuples .py
@@ -6,10 +6,10 @@ def remove_duplicates(sequence):
     return unique_elements
 
 
-# Test the function
+
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 result = remove_duplicates(input_sequence)
-print(result)  # Output: [2, 3, 4, 5, 6, 7]
+print(result)  
     
     
  
